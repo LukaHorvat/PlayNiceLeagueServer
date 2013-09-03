04ed30447e2580188fd543589c14a0372f9ed5b6
Luka Horvat <lukahorvat9@gmail.com>
2013-09-03 20:51:01 +0200

    Modified the connection string, added procfile

--- storage.js
+++ storage.js
@@ -2,7 +2,7 @@
 
 	var mongoose = require("mongoose");
 
-	mongoose.connect("mongodb://localhost/");
+	mongoose.connect(process.env.MONGOLAB_URI);
 
 	var db = mongoose.connection;
 	db.on('error', console.error.bind(console, 'connection error:'));
